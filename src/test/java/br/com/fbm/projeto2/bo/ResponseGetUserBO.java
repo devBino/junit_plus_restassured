606bd0df29a5f33ac757a08a1828373e4344d076
Fernando Bino Machado <programemachado@gmail.com>
2023-03-23 12:20:46 -0300

    Up initial tests structure

---
+++ src/test/java/br/com/fbm/projeto2/bo/ResponseGetUserBO.java
@@ -0,0 +1,39 @@
+package br.com.fbm.projeto2.bo;
+
+import java.io.Serializable;
+
+/**
+ * {@code ResponseGetUserBO} given a 
+ * response BO when we receive
+ * users response from the reqres.in
+ * after request an user by id
+ *
+ * @author Fernando Bino Machado
+ */
+public class ResponseGetUserBO implements Serializable {
+
+	private static final long serialVersionUID = 4799846180383383708L;
+	
+	private UserBO data;
+	private SuportBO support;
+	
+	public ResponseGetUserBO() {}
+
+	public UserBO getData() {
+		return data;
+	}
+
+	public void setData(UserBO data) {
+		this.data = data;
+	}
+
+	public SuportBO getSupport() {
+		return support;
+	}
+
+	public void setSupport(SuportBO support) {
+		this.support = support;
+	}
+	
+	
+}
